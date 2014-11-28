51fe11a5ded222839ded82ab97861978bf89b5d1
Mickael Goubin <micky0304@gmail.com>
2014-11-28 12:38:25 +0100

    better management of networking exceptions

--- src/networking/UdpReceiver.java
+++ src/networking/UdpReceiver.java
@@ -11,16 +11,22 @@ import java.net.SocketException;
  * Created by MagicMicky on 28/11/2014.
  */
 public class UdpReceiver extends Thread {
-    private boolean shouldRun = true;
     private final ChatNI chatNi;
     private final DatagramPacket datagramPacket;
     private final byte[] buffer;
     private DatagramSocket receiveSocket;
-    public UdpReceiver(ChatNI chatNi) throws SocketException {
+    private boolean shouldRun;
+
+    public UdpReceiver(ChatNI chatNi) throws NetworkingException.ReceivingException {
         this.chatNi = chatNi;
-        receiveSocket=new DatagramSocket(Conf.PORT);
+        try {
+            receiveSocket=new DatagramSocket(Conf.PORT);
+        } catch (SocketException e) {
+            throw new NetworkingException.ReceivingException("Error while initializing the receiving socket with port " + Conf.PORT, e);
+        }
         this.buffer = new byte[2048];
         this.datagramPacket = new DatagramPacket(buffer, buffer.length);
+        this.shouldRun=true;
     }
     @Override
     public void run() {
