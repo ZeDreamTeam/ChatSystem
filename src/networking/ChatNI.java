77f2a9dec2e93c7a1f64d8f67c8c8f5dd1d3caaa
Mickael Goubin <micky0304@gmail.com>
2014-11-28 10:50:24 +0100

    Update chatNi with all the methods to send messages

--- src/networking/ChatNI.java
+++ src/networking/ChatNI.java
@@ -1,15 +1,14 @@
 package networking;
 
-import data.HelloAckMessage;
-import data.HelloMessage;
+import data.*;
+import messages.MessageFactory;
 
 /**
  * Created by MagicMicky on 28/11/2014.
  */
 public class ChatNI {
-
+    private MessageFactory factory;
     public ChatNI() {
-
     }
     public void performSendHello(HelloMessage helloMessage) {
         //TODO : parser helloMessage
@@ -18,6 +17,19 @@ public class ChatNI {
     public void performSendHelloAck(HelloAckMessage helloAckMessage) {
         //TODO
     }
+
+    public void performSendGoodbyeMessage(GoodbyeMessage message) {
+        //TODO
+    }
+
+    public void performSendMessMessage(MessMessage message) {
+
+    }
+
+    public void performSendMessAckMessage(MessAckMessage message) {
+
+    }
+
     public void doReceive(String message) {
         //TODO: retrieve type
         //TODO: unparse Message
