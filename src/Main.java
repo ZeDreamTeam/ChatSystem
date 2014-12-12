964228183df529f50aff0781832e63548be33974
Mickael <micky0304@gmail.com>
2014-12-12 13:06:56 +0100

    Better starting the application

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import controller.Controller;
-import data.User;
 import gui.GUI;
-import gui.WindowLogIn;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.net.UnknownHostException;
 
 /**
  * Created by MagicMicky on 28/11/2014.
@@ -13,12 +8,12 @@ import java.net.UnknownHostException;
 public class Main extends Application{
     public static void main(String[] params){
 
-
         launch(params);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         GUI gui = new GUI();
+        gui.start(stage);
     }
 }
